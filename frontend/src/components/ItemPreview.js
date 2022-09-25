09e706ef0806717f6f7644d0ef7687947725fc3f
Eltnux71 <elton.peake71@gmail.com>
2022-09-25 18:32:43 +0100

    Incident fix

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import agent from "../agent";
 import { connect } from "react-redux";
 import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
-import altImg from "../imgs/placeholder.png";
+import placeholder from "../imgs/placeholder.png"  
 
 const mapDispatchToProps = (dispatch) => ({
   favorite: (slug) =>
@@ -38,7 +38,7 @@ const ItemPreview = (props) => {
       <img
         alt="item"
         src={item.image}
-        onError={(e)=>{e.target.onerror = null; e.target.src=altImg}}
+        onError={(e)=>{e.target.onerror = null; e.target.src=placeholder}}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
